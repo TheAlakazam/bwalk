2cb9dab5ad3e709b2ab7f05989f1ec4ce2d2c104
Piyush Jaipuriyar <piyushjaipuriyar@gmail.com>
2018-02-02 18:54:06 +0530

    Added prone back

--- rob.py
+++ rob.py
@@ -10,6 +10,15 @@ print(ports)
 dxl = pypot.dynamixel.DxlIO(ports[0])
 
 dxl.set_moving_speed({1:70, 2:70, 3:100, 4:100})
+def prone_back():
+    dxl.set_moving_speed({1:50, 2:50, 3:50, 4:50, 5:50, 6:50, 7:50, 8:50})
+    dxl.set_goal_position({1:90, 2:-90, 7:90, 8:-90, 5:-150, 6:150})
+    time.sleep(2)
+    dxl.set_goal_position({7:0, 8:0, 3:-30, 4:30})
+    time.sleep(2)
+    dxl.set_goal_position({3:0, 4:0})
+    time.sleep(2)
+    dxl.set_goal_position({1:0, 2:0, 5:0, 6:0, 7:45, 8:-45})
 
 def back():
     dxl.set_goal_position({5:0, 6:0, 7:18, 8:-18})
@@ -94,7 +103,7 @@ def dpad(pos):
     elif pos.right:
         right()
     elif pos.middle:
-        print("fire")
+        prone_back()
 bd = BlueDot()
 bd.when_pressed = dpad
 pause()
--- sd.py
+++ sd.py
@@ -6,31 +6,13 @@ dxl = pypot.dynamixel.DxlIO(ports[0])
 
 
 def back():
-    dxl.set_goal_position({5:0, 6:0, 7:18, 8:-18})
-    dxl.set_moving_speed({1:100, 2:100, 3:100, 4:100})
-    a,b = -25,-35
-    c = 40
-    dxl.set_goal_position({3:a, 4:b, 1:0})
-    time.sleep(0.3)
-    dxl.set_goal_position({1:c})
-    time.sleep(0.3)
-    dxl.set_goal_position({4:0})
-    time.sleep(0.8)
-    dxl.set_goal_position({3:0, 1:0})
-    time.sleep(0.3)
-    b,a = 25,35
-    dxl.set_goal_position({5:0, 6:0, 7:18, 8:-18})
-    c = -40
-    dxl.set_goal_position({3:a, 4:b, 2:0})
-    time.sleep(0.3)
-    dxl.set_goal_position({2:c})
-    time.sleep(0.3)
-    dxl.set_goal_position({3:0})
-    time.sleep(0.8)
-    dxl.set_goal_position({2: 0, 4: 0})
-    time.sleep(0.3)
-    dxl.set_goal_position({1:0, 2:0, 3:0, 4:0})
-
-while True:
-    back()
+    dxl.set_moving_speed({1:50, 2:50, 3:50, 4:50, 5:50, 6:50, 7:50, 8:50})
+    dxl.set_goal_position({1:90, 2:-90, 7:90, 8:-90, 5:-150, 6:150})
+    time.sleep(5)
+    dxl.set_goal_position({7:0, 8:0, 3:-30, 4:30})
+    time.sleep(2)
+    dxl.set_goal_position({3:0, 4:0})
+    time.sleep(2)
+    dxl.set_goal_position({1:0, 2:0, 5:0, 6:0, 7:45, 8:-45})
+back()
 
