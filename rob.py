5c29089716d9b7e0b7d6f6b624448b0699c5b7ec
Gunjan Chourasia <gchourasia123@gmail.com>
2018-02-02 18:36:42 +0530

    Added forward

--- rob.py
+++ rob.py
@@ -66,6 +66,23 @@ def right():
     time.sleep(1)
     dxl.set_goal_position({2: 0, 4: 0})
     time.sleep(1)
+
+
+def forward():
+    dxl.set_goal_position({1:0,2:0,3:0,4:0})
+    dxl.set_goal_position({7:70,8:-70})
+    c,d=-27,-27
+    a,b=-22,-22
+    while (True):
+        dxl.set_moving_speed({1:100,2:100,3:100,4:100})
+        dxl.set_goal_position({3:a,4:b})
+        time.sleep(0.3)
+        a,b=-a,-b
+        dxl.set_goal_position({1:c,2:d})
+        c,d=-c,-d
+        time.sleep(0.3)
+        dxl.set_goal_position({1:0,2:0,3:0,4:0})
+
 def dpad(pos):
     if pos.top:
         print("Up")
